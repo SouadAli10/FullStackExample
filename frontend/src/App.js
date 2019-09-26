11f439c285f157b9276204e05ea4b35d1e3b84b5
Souad <Souad@polarstork.com>
2019-09-26 23:58:48 +0300

    Add React Routers Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import EmployeeList from './Components/Employeelist'
+import CreateEmployee from './Components/CreateEmployee'
+import EditEmployee from './Components/EditCurrentEmployee'
+import Navbar from './Components/NavBar'
 function App() {
   return (
-    <div className="App">
-     <p>vfdifu</p>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={EmployeeList} />
+      <Route path="/edit/:id" component={EditEmployee} />
+      <Route path="/create" component={CreateEmployee} />
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
