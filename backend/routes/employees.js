c7f33f68aa37609d0db2f4bfdb2c27917f20e532
Souad <Souad@polarstork.com>
2019-09-26 23:53:53 +0300

    Add Filter, Pagination and Search API

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -4,35 +4,103 @@ let Employee = require('../datamodels/employees.models');
 ///Create the get http request 
 
 router.route('/').get((req, res) => {
-    Employee.find()
-      .then(Employee => res.json(Employee))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+  var pageNo = parseInt(req.query.pageNo)
+  var size = parseInt(req.query.size)
+  var department = req.query.department
+  var search = req.query.name
+  var query = {}
+  if (pageNo < 0 || pageNo === 0) {
+    var response = {
+      "error": true,
+      "message": "invalid page number, should start with 1"
+    };
+    return res.json(response)
+  }
+  query.skip = size * (pageNo - 1)
+  query.limit = size
+  // query.department = department
+  // if (query.department = "all"){
+  //   delete query.department
+  // }
+  // Find some documents
+  
+  Employee.countDocuments({}, function (err, totalCount) {
+    if (err) {
+      response = {
+        "error": true,
+        "message": "Error fetching data"
+      }
+    }
+    else if(department !== "" && department !== null){
+      Employee.find({department: department, name: { $regex: search, $options: "$"}}, null, query, function (err, data) {
+        // Mongo command to fetch all data from collection.
+        if (err) {
+          var response = {
+            "error": true,
+            "message": "Error fetching data"
+          };
+        } else {
+          var totalPages = Math.ceil(totalCount / size)
+         var  response = {
+            "error": false,
+            "employees": data,
+            "pages": totalPages,
+            "count": totalCount
+          };
+        }
+        res.json(response);
+      })
+    }
+    else  {
+      Employee.find({ name: { $regex: search, $options: "$"}}, null, query, function (err, data) {
+        // Mongo command to fetch all data from collection.
+        if (err) {
+          var response = {
+            "error": true,
+            "message": "Error fetching data"
+          };
+        } else {
+          var totalPages = Math.ceil(totalCount / size)
+         var  response = {
+            "error": false,
+            "employees": data,
+            "pages": totalPages,
+            "count": totalCount
+          };
+        }
+        res.json(response);
+      })
+    }
+
+  })
+})
+
+
 
 ///create the http post request
 
 router.route('/add').post((req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const phoneNumber = Number(req.body.phoneNumber);
-    const dateofBirth = Date.parse(req.body.dateofBirth);
-    const title = req.body.title;
-    const imagesource = req.body.imagesource;
-    const department = req.body.department;
+  const name = req.body.name;
+  const email = req.body.email;
+  const phoneNumber = req.body.phoneNumber;
+  const dateofBirth = Date.parse(req.body.dateofBirth);
+  const title = req.body.title;
+  const imagesource = req.body.imagesource;
+  const department = req.body.department;
 
-    const newEmployee = new Employee({
-        name,
-        email,
-        phoneNumber,
-        dateofBirth,
-        title,
-        imagesource,
-        department
-    });
+  const newEmployee = new Employee({
+    name,
+    email,
+    phoneNumber,
+    dateofBirth,
+    title,
+    imagesource,
+    department
+  });
 
-    newEmployee.save()
-        .then(() => res.json('Employee added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+  newEmployee.save()
+    .then(() => res.json('Employee added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 })
 
 ///create an http request to fetch an employee by ID
@@ -53,7 +121,7 @@ router.route('/update/:id').post((req, res) => {
     .then(Employee => {
       Employee.name = req.body.name;
       Employee.email = req.body.email;
-      Employee.phoneNumber = Number(req.body.phoneNumber);
+      Employee.phoneNumber = req.body.phoneNumber;
       Employee.dateofBirth = Date.parse(req.body.dateofBirth);
       Employee.title = req.body.title;
       Employee.imagesource = req.body.imagesource;
@@ -66,6 +134,6 @@ router.route('/update/:id').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-  
+
 ///export the module api for use in the future
 module.exports = router;
